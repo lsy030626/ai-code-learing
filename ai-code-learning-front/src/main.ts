76ea01dd1b14d69afb9cf4600f9a9f2f3df071ca
lsy <3086603619@qq.com>
2026-03-04 16:58:37 +0800

    添加组件库

--- ai-code-learning-front/src/main.ts
+++ ai-code-learning-front/src/main.ts
@@ -5,10 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// 组件库
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
